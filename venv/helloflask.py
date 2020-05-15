a0767de2ec607b12fb3a4ceb71acdd0be5f4e6da
Harir13 <hariharan.ramamoorthy@gmail.com>
2020-05-15 17:16:52 -0400

    Flask Page with Graphviz that runs from a desktop file import of a process

--- venv/helloflask.py
+++ venv/helloflask.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
 from graphviz import Digraph
 
-from bokeh.client import pull_session
-from bokeh.embed import server_session
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+
+import io
+import base64
 
 import pandas as pd
 import numpy as np
+
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -36,7 +42,7 @@ def svgtest():
   dot1.graph_attr["newrank"] = "true"
   dot1.graph_attr['outputorder'] ='edgesfirst'
   dot1.graph_attr['rankdir'] = 'LR'
-  dot1.graph_attr['shape'] = 'plaintext'
+  dot1.graph_attr['shape'] = 'record'
   dot1.graph_attr['size'] = "12.75,7.25"
   dot1.graph_attr['fontsize'] = "12"
 
@@ -48,9 +54,16 @@ def svgtest():
       with dot1.subgraph(name=cname) as c:
           c.attr(style='filled', color='lightgrey',fontsize='20',label=str(owners))
           for index, row in processi.iterrows():
-              c.node(row['Process Step From'],,shape='box',width='3', height='.5',fixedsize='true',style='filled', color=row['colors'],fontsize='20')
+              c.node(row['Process Step From'])
+              c.node_attr['shape']='box'
+              c.node_attr['width']='3'
+              c.node_attr['height'] = '.5'
+              c.node_attr['fixedsize'] = 'true'
+              c.node_attr['style'] = 'filled'
+              c.node_attr['fontsize'] = '20'
+              #c.node_attr['color'] = 'lightgrey'
               c.node_attr['color'] = row['colors']
-              #c.node_attr['href'] = '#'
+              c.node_attr['href'] = '#'
   for i, val in enumerate(steps):
       if i == 0:
           prev = val
@@ -61,13 +74,37 @@ def svgtest():
           prev = val
   chart_output1 = dot1.pipe(format='svg').decode("utf-8")
 
+  fig = plt.figure(figsize=(15, 10))
+  ax = fig.add_subplot(1, 1, 1)
+  ax.set_xlabel('Process Step', fontsize=15)
+  ax.set_ylabel('Step Effort', fontsize=15)
+  ax.set_title('Load Chart', fontsize=20)
+
+
   return render_template('svgtest.html', chart_output1=chart_output1)
 
-@app.route("/VSM")
+@app.route("/VSM",methods=['GET'])
 def vsm():
-    pass
+    process = pd.read_excel('/Users/hari/Desktop/Process.xlsx')
+    process['Takttime'] = process['Effort'].sum() / len(process)
+    process['colors'] = np.where(process['Takttime'] >= process['Effort'], 'green', 'orange')
 
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    for i, val in process.iterrows():
+        axis.bar(val['Owner'], val['Effort'], color=val['colors'])
 
+    axis.axhline(y=36.5, color='green', linestyle='-')
+
+    # Convert plot to PNG image
+    pngImage = io.BytesIO()
+    FigureCanvas(fig).print_png(pngImage)
+
+    # Encode PNG image to base64 string
+    pngImageB64String = "data:image/png;base64,"
+    pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
+
+    return render_template("vsm.html", image=pngImageB64String)
 
 if __name__ == "__main__":
     app.run(debug=True)
